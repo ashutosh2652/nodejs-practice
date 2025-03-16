cb9c0f0c179caa432857a6c152811af60a60e113
ASHUTOSH <xyz@gmail.com>
2025-03-16 21:05:32 +0530

    File creater added

---
+++ file_creater.js
@@ -0,0 +1,21 @@
+import readline from "readline";
+import fs from "fs";
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const filecreation = () => {
+  rl.question("Enter your filename:", (filename) => {
+    rl.question("Enter content of your file:", (content) => {
+      fs.writeFile(`${filename}.txt`, content, (err) => {
+        if (err) {
+          console.log("Error writing the file.");
+        } else {
+          console.log(`File ${filename}.txt created successfully.`);
+        }
+      });
+      rl.close();
+    });
+  });
+};
+filecreation();
